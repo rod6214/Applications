52b0957ec20b33d1b2270006959d307d83b2dbd2
NELSON <NELSON@NELSON-PC>
2018-06-18 00:38:02 -0300

    .

--- Applications/pthread_test/libraries.h
+++ Applications/pthread_test/libraries.h
@@ -1,5 +1,12 @@
 #pragma once
 #ifndef  LIBRARIES_H
 #define LIBRARIES_H
-
+#include<iostream>
+#include"config.h"
+#include"sched.h"
+#include"context.h"
+#include"need_errno.h"
+#include"pthread.h"
+#include"implement.h"
+#include"semaphore.h"
 #endif // ! LIBRARIES_H
--- Applications/pthread_test/main.cpp
+++ Applications/pthread_test/main.cpp
@@ -1,6 +1,19 @@
 #include"libraries.h"
 
+void* ejemplo01(void* arg) 
+{
+	std::cout << "ejemplo01" << std::endl;
+	return nullptr;
+}
+
 int main(int argc, char *argv[]) 
 {
+	pthread_t th;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	pthread_create(&th, &attr, ejemplo01, 0);
+	pthread_join(th, 0);
+	pthread_attr_destroy(&attr);
 	return 0;
 }
